0d9c598701cf2f843628121669a3805367d0f4ea
Vadim Bespal <bespvvadim@gmail.com>
2019-04-22 16:21:28 +0300

    Something workd already

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// var editor = new EditorJS({
+//   holderId : 'editorjs',
+//   tools: {
+//        header: {
+//          class: Header,
+//          inlineToolbar : true
+//        },
+//        // ...
+//    },
+//
+// });
